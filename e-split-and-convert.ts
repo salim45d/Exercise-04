0bbb264d753452cd448176c0d2c0af7fe6301731
salim45d <salim.agus@live.com>
2024-09-05 09:29:36 +0800

    feat: Split String and Convert to Array

---
+++ e-split-and-convert.ts
@@ -0,0 +1,9 @@
+/////////////////////////////////
+//SPLIT STRING CONVERT TO ARRAY//
+/////////////////////////////////
+
+function splitStringConvert(word: string) {
+  return word.split(" ");
+}
+
+console.log(splitStringConvert("Hello World"));
